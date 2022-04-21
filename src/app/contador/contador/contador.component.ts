0eb6b7ea25874bcfa41c597a1c9b70f80c3fe619
Matias Foresto <matiasforesto07@gmail.com>
2022-04-21 15:50:39 -0300

    Fin seeccon 4

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -2,13 +2,7 @@ import  {Component} from '@angular/core';
 
 @Component({
     selector: 'app-contador',
-    template: `
-        <h1>{{title}}</h1>
-        <h3>La base es:<strong>{{base}}</strong></h3>
-        <button (click)="acumular(-base)"> -{{base}}</button>
-        <span>{{numero}}</span>   
-        <button (click)="acumular(+base)"> +{{base}}</button>
-    `
+    templateUrl:'contador.component.html'
 })
 
 export class ContadorComponent{
